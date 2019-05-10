bc1e7c8661db0c771bf487d6b3b29d5536a37ba4
omar <omasrt@gmail.com>
2019-05-10 17:13:45 -0500

    adds addition and subtraction logic

---
+++ js/main.js
@@ -0,0 +1,47 @@
+
+//const countArr = []; 
+
+var count;
+
+const plusEl = document.getElementById('plus');
+const minusEl = document.getElementById('minus');
+const countEl = document.getElementById('count');
+const inputEl = document.getElementById('input');
+
+document.getElementById('plus').addEventListener('click', handlePlus);
+document.getElementById('minus').addEventListener('click', handleMinus);
+
+
+
+
+
+
+init();
+
+function handlePlus(evt) {
+  let inputValue = document.getElementById('input').value;
+  valueNum = parseInt(inputValue, 10);
+  count = valueNum + count;
+  render();
+
+}
+
+function handleMinus(evt) {
+  let inputValue = document.getElementById('input').value;
+  valueNum = parseInt(inputValue, 10);
+  count = count - valueNum;
+  render();
+}
+
+function render() {
+  countEl.textContent = count;
+}
+
+
+
+
+function init() {
+  count = 0;
+  render();
+
+}
